916d8c1bcb8bb093b57c6043aae9a337958482a2
Xiaoquan Kong <u1mail2me@gmail.com>
2019-08-01 12:51:34 +0800

    Update

--- tensorflow_addons/losses/crf_losses.py
+++ tensorflow_addons/losses/crf_losses.py
@@ -1,6 +1,7 @@
 from tensorflow.python.keras.losses import sparse_categorical_crossentropy, \
     categorical_crossentropy
 
+import tensorflow as tf
 from tensorflow_addons.utils import keras_utils
 
 
@@ -14,7 +15,6 @@ def crf_nll(y_true, y_pred):
     return nloglik
 
 
-@keras_utils.register_keras_custom_object
 def crf_loss(y_true, y_pred):
     # TODO: change to tf 2.0 class based implementation
     crf, idx = y_pred._keras_history[:2]
@@ -26,3 +26,19 @@ def crf_loss(y_true, y_pred):
             return sparse_categorical_crossentropy(y_true, y_pred)
         else:
             return categorical_crossentropy(y_true, y_pred)
+
+
+@keras_utils.register_keras_custom_object
+class CrfLoss(tf.keras.losses.Loss):
+    def __init__(self,
+                 reduction=tf.keras.losses.Reduction.NONE,
+                 name='crf_loss'):
+        super(CrfLoss, self).__init__(name=name, reduction=reduction)
+
+    def call(self, y_true, y_pred):
+        return crf_loss(y_true, y_pred)
+
+    def get_config(self):
+        config = {}
+        base_config = super(CrfLoss, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
